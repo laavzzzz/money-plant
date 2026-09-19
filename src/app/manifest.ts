e5436316a586a3dcea528ab1d5b2f63bf7b41fa3
laavzzzz <laveezazafar1910@gmail.com>
2026-09-19 23:17:37 +0530

    fix(manifest): update apple-touch-icon source to use JPG format

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -91,7 +91,7 @@ export default function manifest(): MetadataRoute.Manifest {
         purpose: "maskable",
       },
       {
-        src: "/icons/apple-touch-icon.png",
+        src: "/icons/apple-touch-icon.jpg",
         sizes: "180x180",
         type: "image/png",
       },
